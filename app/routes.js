b6f9aee5680f8c15e54eb1a612d0950ea1ed6c80
shubhamkucheria <shubhamkucheria@gmail.com>
2019-03-02 04:52:45 -0500

    add ui for word hack base

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
 
-let jsonData = require('./words.json');
+let jsonData = require('./word.json');
 function generateAnagrams(word) {
     if (word.length < 2) {
         return [word];
@@ -25,12 +25,18 @@ function generateAnagrams(word) {
 
 /* Execution & Benchmarking */
 function benchMark(word){
-    console.time("time taken");
     var result = generateAnagrams(word);
-    console.log(jsonData.filter(x => result.includes(x)));
-    console.timeEnd("time taken");
+    return jsonData.filter(x => result.includes(x));
 }
 
-benchMark("tac");
 
-
+module.exports = function (app) {
+    app.post('/api/todo', function (req, res) {
+    	var result = new Promise(function(resolve, reject) {
+			resolve(benchMark(req.body.text));
+		});
+		result.then(function(value) {
+			res.json({result: value});
+		});        
+    });
+};
