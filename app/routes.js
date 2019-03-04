ed5461e7426b3eebe9d9455cc25be2a5129de246
Shubham Kucheria <Shubhamkucheria@gmail.com>
2019-03-04 12:00:38 +0530

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,20 +1,20 @@
 
-let jsonData = require('./word.json');
+let jsonData = require('./wordByLength.json');
 function generateAnagrams(word) {
     if (word.length < 2) {
         return [word];
     } else {
-        var anagrams = [];
-        var before, focus, after;
-        var shortWord, subAnagrams, newEntry;
-        var i = 0;
-        for (var i = 0; i < word.length; i++) {
+        let anagrams = [];
+        let before, focus, after;
+        let shortWord, subAnagrams, newEntry;
+        let i = 0;
+        for (let i = 0; i < word.length; i++) {
             before = word.slice(0, i);
             focus = word[i];
             after = word.slice(i + 1, word.length + 1);
             shortWord = before + after;
             subAnagrams = generateAnagrams(shortWord);
-            for (var j = 0; j < subAnagrams.length; j++){
+            for (let j = 0; j < subAnagrams.length; j++){
                 newEntry = focus + subAnagrams[j];
                 anagrams.push(newEntry);
             }
@@ -23,17 +23,50 @@ function generateAnagrams(word) {
     }
 }
 
-/* Execution & Benchmarking */
-function benchMark(word){
+function allPossibleCombinations(input, length, curstr) {
+    if(curstr.length == length) return [ curstr ];
+    var ret = [];
+    for(var i = 0; i < input.length; i++) {
+        ret.push.apply(ret, allPossibleCombinations(input, length, curstr + input[i]));
+    }
+    return ret;
+}
+
+
+function benchMark(word, limit){
     var result = generateAnagrams(word);
-    return jsonData.filter(x => result.includes(x));
+    let objKey = word.length;
+    if(limit && (limit < objKey) ) {
+        objKey = limit;
+        var pp = allPossibleCombinations(word.split(""), limit, '');
+        pp.sort(function(a, b){return a.length - b.length});
+        var objOfWord = {};
+        pp.forEach((item) => {
+            if(objOfWord[item.length]) {
+                objOfWord[item.length].push(item);
+            } else {
+                objOfWord[item.length] = [];
+                objOfWord[item.length].push(item);
+            } 
+        });
+        return jsonData[objKey].filter(x => objOfWord[objKey].includes(x));
+    } else {
+        if(jsonData[objKey] > result.length) {
+            return [...new Set(jsonData[objKey].filter(x => result.includes(x)))];
+        } else {
+            return [...new Set(result.filter(x => jsonData[objKey].includes(x)))];
+        }
+    }
 }
 
 
 module.exports = function (app) {
     app.post('/api/todo', function (req, res) {
-    	var result = new Promise(function(resolve, reject) {
-			resolve(benchMark(req.body.text));
+    	let result = new Promise(function(resolve, reject) {
+
+            let finalWords = benchMark(req.body.text, req.body.length);
+            console.log(finalWords);
+            resolve(finalWords);
 		});
 		result.then(function(value) {
 			res.json({result: value});
